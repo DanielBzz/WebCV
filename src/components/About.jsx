545a73b2aa8131e7d134458d969584fe42963fa6
Daniel Bzz <daniel6966@gmail.com>
2023-10-31 01:28:03 +0200

    Add about part

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,20 +4,31 @@ import "../css/About.css";
 export default function About(){
 
     return <div>
-        <h1>Hey, I'm Daniel</h1>
-        <p>A software Developer</p>
-        <p>
-        I'm a passionate software developer with a keen interest in architecture and a penchant for tackling complex challenges. 
-        Armed with a degree in Computer Science, I'm poised to embark on my journey in the tech industry and eager to contribute my skills to innovative projects.
-        My approach to development is deeply rooted in Object-Oriented Programming principles, which I believe form the cornerstone of robust and scalable software solutions. 
+    <div id="aboutContainer">
+        <h1 id="aboutTitle" className="titleItem item">Hey, I'm Daniel.</h1>
+        <p id="aboutSubTitle" className="titleItem item">A human && SW Developer</p>
+        <p id="about" className="item">
+        I'm a passionate software developer with a keen interest in architecture and a penchant for tackling complex challenges.<br/> 
+        Armed with a degree in Computer Science, I'm poised to embark on my journey in the tech industry and eager to contribute my skills to innovative projects.<br/>
+        My approach to development is deeply rooted in Object-Oriented Programming principles, which I believe form the cornerstone of robust and scalable software solutions.<br/> 
         I'm driven by a deep desire to create software that not only works seamlessly but also stands the test of time.
-        </p>        
-        <h2>My Skills</h2>
+        </p>
+        <div id="ifStatement" className="item">
+        <p>
+            if (lookingForDedicatedProgrammer)<br/>
+            {"{"}<br/>
+            <a href="mailto:danielbaz199@gmail.com">&nbsp;&nbsp;&nbsp;send me message;</a><br/>
+            {"}"}<br/>
+        </p>
+        </div>
+    </div>
+    <div id="skillsContainer">        
+        <h2 className="titleItem">My Skills</h2>
         <SkillType title="Programming languange" skills={["Java", "C#", "Python", "C", "C++", "JavaScript", "CSS", "HTML"]}/>
         <SkillType title="Frameworks" skills={["Spring", "Django", ".NET", "React",]}/>
         <SkillType title="Dev tools" skills={["Git & GitHub & GitHub Actions", "Docker", "Tomcat-servlets", "AWS", "Postman", "Linux", "SQL" ]}/>
         <SkillType title="Familiar concepts" skills={["Object oriented programming", "Design pattenrs", "Multi-Threading", "Test-driven development", "CI/CD" ]}/>
-
+    </div>
     </div>;
 }
 
@@ -26,8 +37,9 @@ function SkillType(props){
     return <div>
     <h5>{props.title}</h5>
     <ul>
-        {props.skills.map(x => <li>{x}</li>)}
+        {props.skills.map((x, ind) => <li key={ind}>{x}</li>)}
     </ul>
     </div>;
             
 }
+
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,21 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import "../css/NavBar.css";
+import MenuIcon from "../icons/list.svg";
 
 export default function NavBar(){
+    const [sideMenuOn, setSideMenu] = useState(false);
+    const toggleSideMenu = () => setSideMenu(!sideMenuOn);
+    let scrollingPos = window.scrollY;
 
-    return <nav className="navBar">
-        <a href="/"><h1>Daniel Bazis Portfolio</h1></a>
+    function onScroll(){
+        const newPos = window.scrollY;
+        if(!sideMenuOn && newPos > scrollingPos){
+            document.getElementById('navBar').style.top = '-90px';
+        }else{
+            document.getElementById('navBar').style.top = '0';
+        }
+
+        scrollingPos = newPos;
+    }
+
+    window.onscroll = onScroll;
+    window.addEventListener('click', (event) => {
+        if(!event.target.closest('#menuButton'))
+            setSideMenu(false)
+    });
+    window.addEventListener('resize', () => setSideMenu(false));
+
+    return <div><nav className="menu" id="navBar">
+        <a id="side-title" href="/"><h1>Daniel Portfolio</h1></a>
         <ul>
+            <NavNode aClass="circle" name="About" />
+            <NavNode aClass="circle" name="Projects" />
+            <NavNode aClass="circle" name="Education and Experience" />
+            <NavNode aClass="circle" name="Contact" />
+        </ul>
+        <button onClick={toggleSideMenu} id="menuButton"><img alt="menuIcon" src={MenuIcon} /></button>
+    </nav>
+    {sideMenuOn && (
+          <nav className="menu" id="sideMenu">
+            <ul>
             <NavNode name="About" />
             <NavNode name="Projects" />
             <NavNode name="Education and Experience" />
             <NavNode name="Contact" />
-        </ul>
-    </nav>
+            </ul>
+          </nav>
+        )}
+    </div>
 }
 
 function NavNode(props){
     return <li className="node">
-    <a href={`/${props.name}`}>{props.name}</a>
+    <a className={props.aClass} href={`/${props.name}`}>{props.name}</a>
     </li>
 }
