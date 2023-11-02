17011df3d321c7d423478c44273d97a873c71971
Daniel Bzz <daniel6966@gmail.com>
2023-11-02 22:25:20 +0200

    almost finish projects part , needs to add animation to div slide and start with footer

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,31 +1,52 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import jsonFiles from "../utils/projectsList.js";
 import "../styles/Projects.css";
 
+
+
 export default function Projects() {
+    const [showProjectId,setShowProjectId] = useState(0);
 
     function onArrowClick(event){
         if(event.target.closest('#leftArrow')){
-
+            switchProject(showProjectId - 1);
         }else{
-
+            switchProject(showProjectId + 1);
         }
-        
     }
-  
+
+    function switchProject(id){
+        const nextId = (id + jsonFiles.length) % jsonFiles.length;
+        
+        document.getElementById('project-' + nextId).style.display = "flex";
+        document.getElementById('project-' + showProjectId).style.display = "none";
+        setShowProjectId(nextId);
+    }
+
+    useEffect(() => {
+        const divs = document.querySelectorAll(".project");
+        const maxHeight = Math.max(...Array.from(divs).map(div =>{
+            console.log(div.clientHeight);
+            return div.clientHeight;
+        } ));
+    
+        document.getElementById("projectContainer").style.height = maxHeight;
+      }, []);
+
     return (
     <div id="Projects" className="mainContainer">
       <h2 className="titleItem">My Projects</h2>
       <div id="leftArrow" className="arrow">
-        <button onClick={onArrowClick}><img alt="left" src={require("../resources/icons/arrow-Left.png")}/></button>
+        <button onClick={onArrowClick}><svg class="svg-icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg"><path d="M590.08 810.666667a42.666667 42.666667 0 0 1-33.28-15.786667l-206.08-256a42.666667 42.666667 0 0 1 0-54.186667l213.333333-256a42.666667 42.666667 0 0 1 65.706667 54.613334L439.04 512l184.32 228.693333a42.666667 42.666667 0 0 1-33.28 69.973334z"  /></svg>
+</button>
       </div>
       <div id="projectContainer">
         {jsonFiles.map((jsonObj, index) => (
-          <Project key={index} json={jsonObj} />
+          <Project id={index} key={index} json={jsonObj} />
         ))}
       </div>
       <div id="rightArrow" className="arrow">
-          <button onClick={onArrowClick}><img alt="right" src={require("../resources/icons/arrow-Right.png")}/></button>
+          <button onClick={onArrowClick}><svg class="svg-icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg"><path d="M426.666667 810.666667a42.666667 42.666667 0 0 1-27.306667-9.813334 42.666667 42.666667 0 0 1-5.546667-60.16L584.96 512 400.64 282.88a42.666667 42.666667 0 0 1 6.4-60.16 42.666667 42.666667 0 0 1 62.293333 6.4l206.08 256a42.666667 42.666667 0 0 1 0 54.186667l-213.333333 256A42.666667 42.666667 0 0 1 426.666667 810.666667z"  /></svg></button>
       </div>
     </div>
   );
@@ -33,16 +54,17 @@ export default function Projects() {
 
 function Project(props) {
   const projectData = props.json;
-  const logo = projectData.logoName;
+  const isInitialProject = props.id === 0;
+
   return (
-    <div className="project borderAddOn">
+    <div style={isInitialProject ? {display : 'flex'} : {}} id={'project-' + props.id} className="project borderAddOn wipe">
       <h2>
         {projectData.name}{" "}
-        {logo.length > 0 && (
-          <img alt="Logo" src={require("../resources/icons/" + logo)} />
+        {projectData.logoName.length > 0 && (
+          <img alt="Logo" src={require("../resources/icons/" + projectData.logoName)} />
         )}
       </h2>
-      {ParagraphFormatter(projectData.description)}
+      {paragraphFormatter(projectData.description)}
       <p>{projectData.tech.length > 0 && projectData.tech}</p>
       <div id="linksContainer">
         {projectData.github.length > 0 && (
@@ -59,12 +81,11 @@ function Project(props) {
   );
 }
 
-function ParagraphFormatter(str) {
-  const parts = str.split("  ");
+function paragraphFormatter(str) {
 
   return (
     <p>
-      {parts.map((str, ind) => (
+      {str.split("  ").map((str, ind) => (
         <span key={ind}>
           {str}
           {str[0] === "-" && <br />}
